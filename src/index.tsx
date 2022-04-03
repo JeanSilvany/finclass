e2c7d2b1b70ba1692c8a766d412bb198c7da6e66
Jean Silvany <jeansilvany@hotmail.com>
2022-04-03 01:32:03 -0300

    feat(components): setup, themes and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { fonts } from "./styles/fonts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 export const App = () => {
+  const [fontsLoaded] = useFonts(fonts);
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 };
 
